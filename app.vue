c3bc2a7ab3b1a1dffdd25f14550c3d247aab38fc
Romain MUR - anthesys <romain.mur@anthesys.fr>
2025-06-23 09:44:56 +0200

    page principale

--- app.vue
+++ app.vue
@@ -1,13 +1,5 @@
 <template>
-<<<<<<< HEAD
   <NuxtLayout>
     <NuxtPage />
   </NuxtLayout>
 </template>
-=======
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
-</template>
->>>>>>> 0a735559203fb409c2273936f6c25ab46fa7c560
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // nuxt.config.ts
 export default defineNuxtConfig({
   css: ['~/assets/css/main.css'],
@@ -23,10 +22,4 @@ export default defineNuxtConfig({
       autoprefixer: {},
     },
   },
-=======
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
->>>>>>> 0a735559203fb409c2273936f6c25ab46fa7c560
 })
